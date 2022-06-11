c2ce83b7cfce4bd2e8c388195bdad97e22af19fe
Viraj Patidar <vrj381@gmail.com>
2022-06-11 09:35:13 +0530

    database setup

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,9 @@ from flask.json import jsonify
 from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from flask import Flask, config, redirect, render_template
 import os
+from src.auth import auth
+from src.bookmarks import bookmarks
+from src.database import db, Bookmark
 
 
 def create_app(test_config=None):
@@ -11,10 +14,20 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
         )
 
     else:
         app.config.from_mapping(test_config)
+
+
+    db.app = app
+    db.init_app(app)
+
+
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
 
 
     @app.get("/")
